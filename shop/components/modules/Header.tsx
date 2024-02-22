9d91315dd875bc4b981775012849b92c3a57564b
Mikhail Saburov <saburov1985@gmail.com>
2024-02-22 21:51:28 +1100

    refactor(Header): добвален перевод кнопки Меню

--- shop/components/modules/Header.tsx
+++ shop/components/modules/Header.tsx
@@ -6,7 +6,9 @@ export const Header = () => {
   return (
     <header className='header'>
       <div className='container header__container'>
-        <button className='btn-reset header__burger'></button>
+        <button className='btn-reset header__burger'>
+          {translations[lang].header.menu_btn}
+        </button>
       </div>
     </header>
   )
