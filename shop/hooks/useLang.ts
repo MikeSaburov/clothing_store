fd012bcdc1fa607cd6dd4231b2ebfa741fb2bb65
Mikhail Saburov <saburov1985@gmail.com>
2024-02-22 21:46:56 +1100

    refactor(useLang): рефактор импортов

--- shop/hooks/useLang.ts
+++ shop/hooks/useLang.ts
@@ -1,5 +1,5 @@
-import { $lang } from '@/context/lang'
 import { useUnit } from 'effector-react'
+import { $lang } from '@/context/lang'
 import translationsJson from '@/public/translations/translations.json'
 
 export const useLang = () => {
