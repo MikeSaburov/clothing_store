9fbfd180dabf5ee1d8ea34681223f54fed8e1fc4
Mikhail Saburov <saburov1985@gmail.com>
2024-02-22 21:48:17 +1100

    refactor(Header): имортировал хук useLang

--- shop/components/modules/Header.tsx
+++ shop/components/modules/Header.tsx
@@ -1,11 +1,12 @@
+import { useLang } from '@/hooks/useLang'
+
 export const Header = () => {
+  const { lang, translations } = useLang()
+
   return (
     <header className='header'>
       <div className='container header__container'>
-        <button className='btn-reset header__burger'>
-          <span></span>
-          <span></span>
-        </button>
+        <button className='btn-reset header__burger'></button>
       </div>
     </header>
   )
