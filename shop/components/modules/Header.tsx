211e55840665a25e77787f5e3f91664e4590c073
Mikhail Saburov <saburov1985@gmail.com>
2024-02-25 21:48:52 +1100

    feature(public/img): добавил иконки меню

--- shop/components/modules/Header.tsx
+++ shop/components/modules/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useLang } from '@/hooks/useLang'
 import { Logo } from '../elements/Logo/Logo'
+import Link from 'next/link'
 
 export const Header = () => {
   const { lang, translations } = useLang()
@@ -16,7 +17,35 @@ export const Header = () => {
           <Logo />
         </div>
 
-        <ul className='header__links'></ul>
+        <ul className='header__links list-reset'>
+          <li className='header__links_item'>
+            <button className='btn-reset header__links_item__btn header__links_item__btn--search'></button>
+          </li>
+          <li className='header__links_item'>
+            <Link
+              href='/favorites'
+              className='header__links_item__btn header__links_item__btn--favorites'
+            ></Link>
+          </li>
+          <li className='header__links_item'>
+            <Link
+              href='/comparison'
+              className='header__links_item__btn header__links_item__btn--compare'
+            ></Link>
+          </li>
+          <li className='header__links_item'>
+            <Link
+              href='/cart'
+              className='header__links_item__btn header__links_item__btn--cart'
+            ></Link>
+          </li>
+          <li className='header__links_item'>
+            <Link
+              href='/profile'
+              className='header__links_item__btn header__links_item__btn--profile'
+            ></Link>
+          </li>
+        </ul>
       </div>
     </header>
   )
