bfa42056685f5b845e3ee278743e612bc80841fd
Mikhail Saburov <saburov1985@gmail.com>
2024-02-26 21:12:35 +1100

    refcator(Menu): добавил useState для пунктов меню и имортировал useLang

--- shop/components/modules/Menu.tsx
+++ shop/components/modules/Menu.tsx
@@ -1,3 +1,10 @@
+import { useLang } from '@/hooks/useLang'
+import { useState } from 'react'
+
 export const Menu = () => {
+  const [showCatalogList, setShowCatalogList] = useState(false)
+  const [showBuyersList, setShowBuyersList] = useState(false)
+  const [showContactsList, setShowContactsList] = useState(false)
+  const { lang, translations } = useLang()
   return <div>Menu</div>
 }
