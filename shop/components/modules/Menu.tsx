44cd9e91559c08ce67387c2c0a20afc71612f179
Mikhail Saburov <saburov1985@gmail.com>
2024-02-26 22:20:27 +1100

    feature(Menu): реализовано закрытие меню и добавлена иконка Закрыть

--- shop/components/modules/Menu.tsx
+++ shop/components/modules/Menu.tsx
@@ -1,6 +1,8 @@
-import { $menuIsOpen } from '@/context/modals'
+import { $menuIsOpen, closeMenu } from '@/context/modals'
 import { useLang } from '@/hooks/useLang'
+import { removeOverflowHiddenFromBody } from '@/lib/utils/common'
 import { useUnit } from 'effector-react'
+
 import { useState } from 'react'
 
 export const Menu = () => {
@@ -10,8 +12,17 @@ export const Menu = () => {
   const { lang, translations } = useLang()
   const menuIsOpen = useUnit($menuIsOpen)
 
+  const handleCloseMenu = () => {
+    removeOverflowHiddenFromBody()
+    closeMenu()
+  }
+
   return (
     <nav className={`nav-menu ${menuIsOpen ? 'open' : 'close'}`}>
+      <button
+        className={`btn-reset nav-menu__close ${menuIsOpen ? 'open' : ''}`}
+        onClick={handleCloseMenu}
+      ></button>
       <h1>Menu</h1>
     </nav>
   )
